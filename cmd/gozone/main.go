f69ead8461eff76963fe69569b58514debcb34ec
Isu <natantrojak@wp.pl>
2025-05-27 19:32:39 +0200

    Imported domain in GoZone main.go

--- cmd/gozone/main.go
+++ cmd/gozone/main.go
@@ -1,5 +1,9 @@
 package main
 
+import "github.com/isudin/gozone/internal/domain"
+
 func main() {
 	println("Welcome to the Zone, stalker.")
+	player := domain.Player{Name: "Test"}
+	println("Good hunting, " + player.Name)
 }
