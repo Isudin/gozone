331d55e12c0f9bd6356b0a220fb369c1551436a1
Isu <natantrojak@wp.pl>
2025-06-01 14:50:56 +0200

    Added initializing db queries

--- cmd/gozone/main.go
+++ cmd/gozone/main.go
@@ -2,14 +2,36 @@ package main
 
 import (
 	"bufio"
+	"context"
+	"database/sql"
+	"log"
 	"os"
 
 	"github.com/isudin/gozone/cmd/gozone/commands"
 	"github.com/isudin/gozone/internal/domain"
+	"github.com/isudin/gozone/internal/repository/sqlc"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	queries := initDatabase()
+	// TODO: Move CreateFaction to init_command.go
+	queries.CreateFaction(context.Background(), "Test")
 	commands.Commands["help"].Exec(nil)
+}
+
+func initDatabase() *sqlc.Queries {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+
+	db, err := sql.Open("postgres", os.Getenv("DB_URL"))
+	if err != nil {
+		log.Fatal("could not connect to database\n", err)
+	}
+	return sqlc.New(db)
 }
 
 func getPlayerData() {
